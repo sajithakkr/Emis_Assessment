1de1dcef0253e96b009568bf5f8c5004f7622c55
sajithakkr <saji.krishnanivas@gmail.com>
2022-03-30 13:37:34 +0530

    removed ARN

--- backend.tf
+++ backend.tf
@@ -4,8 +4,5 @@ terraform {
     key = "main"
     region = "us-east-2"
     dynamodb_table = "my-dynamodb-table"
-    access_key = "AKIARJQDAH3RM4XZFA6V"
-    secret_key = "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"
-    role_arn = "arn:aws:iam::089126878946:role/myec2jenkinsrole"
   }
 }
