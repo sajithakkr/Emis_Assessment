5aa13ea8dbada8707235555ef84b7811bd7f08e9
sajithakkr <saji.krishnanivas@gmail.com>
2022-03-30 09:00:53 +0530

    3rd commit key

--- backend.tf
+++ backend.tf
@@ -4,5 +4,7 @@ terraform {
     key = "main"
     region = "us-east-2"
     dynamodb_table = "my-dynamodb-table"
+    access_key = "AKIARJQDAH3RM4XZFA6V"
+    secret_key = "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"
   }
 }
