f3e2bcfac4d135ec5b5f23bd4ecf35283f387623
sajithakkr <saji.krishnanivas@gmail.com>
2022-03-30 11:17:30 +0530

    arn role addition

--- backend.tf
+++ backend.tf
@@ -6,5 +6,6 @@ terraform {
     dynamodb_table = "my-dynamodb-table"
     access_key = "AKIARJQDAH3RM4XZFA6V"
     secret_key = "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"
+    role_arn = "arn:aws:iam::089126878946:role/myec2jenkinsrole"
   }
 }
